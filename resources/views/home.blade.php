08e512bb6427bfd50a14ac59e7f652aab7ec1eb1
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-20 17:13:13 +0200

    connect data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,9 @@
 <section class="container">
     <h1>I nostri prodotti</h1>
     <ul>
-    @foreach ( $products as $product)
-        <li>{{$product['titolo']}}</li>
-
-    @endforeach
+        @foreach ($products as $product)
+            <li>{{$product['title']}}</li>
+        @endforeach
     </ul>
 </section>
 
