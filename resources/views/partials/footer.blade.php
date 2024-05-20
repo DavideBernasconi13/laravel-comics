4d83a2852424642e193455520db6175aaf59812d
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-20 17:01:34 +0200

    footer and import db comics

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,15 @@
-<footer>
-    Sono footer
+<footer class="d-flex">
+    <div class="container d-flex justify-content-between text-light p-0">
+        <a href="#" class="cta">Sign-up now!</a>
+        <div class="link-social d-flex">
+            <a href="#">Follow us</a>
+            <ul class="d-flex">
+                <li><a href="#"><i class="icon fa-brands fa-facebook"></i></a></li>
+                <li><a href="#"><i class="icon fa-brands fa-twitter"></i></a></li>
+                <li><a href="#"><i class="icon fa-brands fa-youtube"></i></a></li>
+                <li><a href="#"><i class="icon fa-brands fa-pinterest-p"></i></a></li>
+                <li> <a href="#"><i class="icon fa-solid fa-location-dot"></i></a></li>
+            </ul>
+        </div>
+    </div>
 </footer>
