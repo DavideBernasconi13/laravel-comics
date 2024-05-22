c672bbeeb48b3ae1a14e9703f7a62edd018dac3a
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-22 14:18:04 +0200

    route home su logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,8 @@
     <!-- Incollato da bootstrap -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="{{ Vite::asset('resources/img/logo.png') }}" alt="Logo"></a>
+            <a class="navbar-brand" href="{{route('home')}}"><img src="{{ Vite::asset('resources/img/logo.png') }}"
+                    alt="Logo"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
